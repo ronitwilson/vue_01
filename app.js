d72ab3950e652ee5a5cc975f2d1213c25373332f
Ronit Wilson <ronit.study@gmail.com>
2023-04-16 17:52:37 +0000

    template code

--- app.js
+++ app.js
@@ -1,45 +1,26 @@
 const app = Vue.createApp({
-    data () {
-        return {
-            title: "the final empire",
-            author: "The new author",
-            age: 45,
-            showBook: true,
-            x:0,
-            y:0,
-            books: [
-                {"title": "The book 0.1", "author": "Ronit", "isFav":false},
-                {"title": "The book of the day", "author": "Ronit", "isFav":false},
-                {"title": "The book of the morning", "author": "Ronit", "isFav":true}
-            ]
-        }
+    data() {
+      return {
+        showBooks: true,
+        books: [
+          { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+          { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+          { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
+        ],
+      }
     },
     methods: {
-        changeTitleToLowerCase(){
-            this.title= this.title.toLowerCase()
-        },
-        signByMe(signer) {
-            this.title=this.title + " signed by " + signer
-        },
-        toggleShowBook(){
-            this.showBook = !this.showBook
-        },
-
-        handleMouseOverEvent(arg, e){
-            console.log("mouse over event")
-            console.log("printing arg ", arg)
-            console.log("printing event ", e)
-        },
-    
-        handleMouseMove(e){
-            console.log("mouse over event")
-            console.log("event type is ", e.type)
-            this.x = e.offsetX
-            this.y = e.offsetY
-        }
+      toggleShowBooks() {
+        this.showBooks =  !this.showBooks
+      },
+      toogleColour() {
+        
+      }
     }
-})
-
-app.mount('#app')
-
-console.log("hello vue")
+  })
+  
+  app.mount('#app')
+  
+  // Challenge - Add to Favs
+  //   - attach a click event to each li tag (for each book)
+  //   - when a user clicks an li, toggle the 'isFav' property of that book
